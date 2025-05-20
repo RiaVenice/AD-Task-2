d452f0370ebbed096a2cd11feaf86c90d969d8e3
RiaVenice <riatcruz.v1@gmail.com>
2025-05-20 10:50:44 +0800

    docs: changed filename

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include('header.php');
+
+
+$pokemonList = [
+    ["name" => "Pikachu", "img" => "img/23.png"],
+    ["name" => "Jigglypuff", "img" => "img/22.png"],
+    ["name" => "Bulbasaur", "img" => "img/24.png"],
+    ["name" => "Vaporeon", "img" => "img/25.png"]
+];
+?>
+
+
+<div class="collection-section">
+    <h2>Your Pokemon Collection</h2>
+    <div class="pokemon-grid">
+        <?php foreach ($pokemonList as $poke): ?>
+            <div class="poke-card">
+                <img src="<?= $poke['img'] ?>" alt="<?= $poke['name'] ?>">
+                <p><a href="details.php?pokemon=<?= urlencode($poke['name']) ?>"><?= $poke['name'] ?></a></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
