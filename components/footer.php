250baa9c6fa840c4c33fa116f2441de18c9908f6
RiaVenice <riatcruz.v1@gmail.com>
2025-05-20 10:56:50 +0800

    docs: changed filename

---
+++ components/footer.php
@@ -0,0 +1,6 @@
+    <hr>
+    <footer>
+        <p>&copy; 2025 Pokémon Fans</p>
+    </footer>
+</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pokémon Collection</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="site-header">
+        <img src="./img/pokeball.png" alt="Pokéball" class="pokeball">
+         <h1>Pokémon</h1>
+    </header>
+   
+    
